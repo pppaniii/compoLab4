b730a66061f600035b1e67dc00c844c6c57afe2f
mpani03 <milkmilkxciii@gmail.com>
2024-08-08 17:13:17 +0700

    from lab 3

---
+++ src/components/EventCard.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { type Event } from '@/types'
+const props = defineProps<{
+  event: Event
+}>()
+// const event = ref ({
+//     id: 5928101,
+//     category: 'animal welfare',
+//     title: 'Cat Adoption Day',
+//     description: 'Find your new feline friend at this event.',
+//     location: 'Meow Town',
+//     date: 'January 28, 2022',
+//     time: '12:00',
+//     petsAllowed: true,
+//     organizer: 'Kat Laydee'
+// })
+</script>
+
+<template>
+  <RouterLink class="event-link" :to="{ name: 'event-detail-view', params: { id: event.id } }">
+  <div class="event-class">
+    <div class="event-card">
+      <h2>{{ props.event.title }}</h2>
+      <span>@{{ props.event.time }} on {{ props.event.date }}</span>
+    </div>
+  </div>
+</RouterLink>
+</template>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+.event-link {
+  text-decoration: none;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/EventDetails.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import Event from '@/types/Event'
+
+const props = defineProps<{
+  event: Event
+}>()
+</script>
+
+<template>
+  <div class="event-details">
+    <p>{{ props.event.category }}</p>
+    <p>{{ props.event.organizer }}</p>
+  </div>
+</template>
+
+<style scoped>
+.event-details {
+  text-align: right;
+  font-size: 16px; /* Adjust the font size as needed */
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+</style>
